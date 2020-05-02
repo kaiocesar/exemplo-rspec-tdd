086129134a790ead221f9f97c4128fb96be85663
Kaio Cesar <tecnico.kaio@gmail.com>
2020-05-02 12:10:20 -0300

    add subtract method

--- spec/calculator/calculator_spec.rb
+++ spec/calculator/calculator_spec.rb
@@ -15,4 +15,19 @@ describe Calculator do
             expect(result).to eq(7)
         end
     end
+
+    context '#subtract' do
+        it 'two positive numbers' do
+            result = subject.subtract 1, 1
+            expect(result).to eq(0)
+        end
+        it 'two negative numbers' do
+            result = subject.subtract -40, -3
+            expect(result).to eq(-37)
+        end
+        it 'one negative and one positive' do
+            result = subject.subtract -10, 12
+            expect(result).to eq(-22)
+        end
+    end
 end
