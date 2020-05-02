782ee52c3b6630f8c13bb1fa361fd8d0fa42ebca
Kaio Cesar <tecnico.kaio@gmail.com>
2020-05-02 11:41:10 -0300

    refactory with context

--- spec/calculator/calculator_spec.rb
+++ spec/calculator/calculator_spec.rb
@@ -1,14 +1,21 @@
 require 'calculator'
 
 describe Calculator do
-    it 'should sum 2 numbers' do
-        calc = Calculator.new
-        result = calc.sum 2, 6
-        expect(result).to eq(8)
-    end
-    it 'should sum 2 negative numbers' do
-        calc = Calculator.new
-        result = calc.sum -3, -10
-        expect(result).to eq(-13)
+    context '#sum' do
+        it 'two positive numbers' do
+            calc = Calculator.new
+            result = calc.sum 2, 6
+            expect(result).to eq(8)
+        end
+        it 'two negative numbers' do
+            calc = Calculator.new
+            result = calc.sum -3, -10
+            expect(result).to eq(-13)
+        end
+        it 'one positive and one negative number' do
+            calc = Calculator.new
+            result = calc.sum -3, 10
+            expect(result).to eq(7)
+        end
     end
 end
